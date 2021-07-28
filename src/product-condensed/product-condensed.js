08d40425ea8c8d0dbc900017c38ef1541afcaf51
susang <susangmree@gmail.com>
2021-07-28 16:27:30 +0545

    first commit

---
+++ src/product-condensed/product-condensed.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import './product-condensed.css';
+import DataService from '../services/data-service';
+
+let ds = new DataService();
+
+
+class ProductCondensed extends Component 
+{
+    constructor(props)
+    {
+        super(props);
+
+        // Bind
+        this.removeProduct = this.removeProduct.bind(this); 
+    }
+
+    removeProduct = () =>
+    {
+        ds.removeWishListItem(this.props.product);
+    }
+
+    render()
+    {
+        return(
+           <li className = "pc-condensed list-group-item ">
+               <p>{this.props.product.title} | <b> ${this.props.product.price} </b> </p>
+               <a className = "btn btn-outline-danger" onClick = {() => this.removeProduct()}>X</a>
+           </li>
+        );
+    }
+}
+
+export default ProductCondensed;
+
